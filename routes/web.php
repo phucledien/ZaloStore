baf51f85993f78f69fb714221d6f5d48618c5bec
Phuc Le Dien <phucledien@gmail.com>
2018-05-28 18:04:50 +0700

    Deleted unecessary Home view

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
