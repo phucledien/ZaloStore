7598364ffb530a9a005cb7140c202fa161a97be4
Phuc Le Dien <phucledien@gmail.com>
2018-05-28 18:03:01 +0700

    Added middleware for admin resources

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer">
     <div class="container">
     <div class="text-center">
-        <small>Copyright © Your Website 2018</small>
+        <small>Copyright © Phuc Le Dien 2018</small>
     </div>
     </div>
 </footer>
@@ -22,7 +22,14 @@
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a class="btn btn-primary" href="login.html">Logout</a>
+        <a class="btn btn-primary" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
         </div>
     </div>
     </div>
